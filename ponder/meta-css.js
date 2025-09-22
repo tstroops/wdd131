e5a26bfcd3a3ac4dd5845087b3fdba558448a4ba
tstroops <tomstroops@gmail.com>
2025-09-22 11:53:41 -0600

    vars and constants

--- ponder/meta-css.js
+++ ponder/meta-css.js
@@ -5,7 +5,10 @@ let area = radius*radius*PI
 console.log(area)
 
 const one = 1;
-const two = '2';
+const two = 2;
+
+let result = one + two
+console.log(result)
 
 
 
@@ -16,4 +19,4 @@ if (true) {
     console.log(student); //works just fine, it's being accessed within the block
 }
 console.log(course); //works fine, course is global
-console.log(student); //does not work, can't access a block variable outside the block
+//console.log(student); //does not work, can't access a block variable outside the block
