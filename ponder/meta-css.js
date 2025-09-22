8c890118c37717e2945fb53520506beac86463ab
tstroops <tomstroops@gmail.com>
2025-09-22 12:29:06 -0600

    there's color changes for h2 now

--- ponder/meta-css.js
+++ ponder/meta-css.js
@@ -20,3 +20,11 @@ if (true) {
 }
 console.log(course); //works fine, course is global
 //console.log(student); //does not work, can't access a block variable outside the block
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+    let highlight = document.getElementById(codeValue).style.color = 'red';
+})
+                
